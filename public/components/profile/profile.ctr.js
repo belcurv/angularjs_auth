8b1185318fc1e00d6c64388e78f6136d16f77db0
JayS <jrschwane@uwalumni.com>
2016-04-10 15:48:14 -0500

    Add profile template and logic

--- public/components/profile/profile.ctr.js
+++ public/components/profile/profile.ctr.js
@@ -21,7 +21,18 @@
         vm.profile = store.get('profile');
         
         function getMessage() {
-            $http.get('http://localhost:3000/api/public')
+            $http.get('http://localhost:3000/api/public', {
+                skipAuthorization: true
+            }).then(function (response) {
+                vm.message = response.data.message;
+            });
+        }
+        
+        function getSecretMessage() {
+            $http.get('http://localhost:3000/api/private')
+            .then(function(response) {
+                vm.message = response.data.message;
+            });
         }
         
     }
