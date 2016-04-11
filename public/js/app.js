9538c24c077bdfa3c0eca8ba67fc4a6c47b31d59
JayS <jrschwane@uwalumni.com>
2016-04-10 22:22:42 -0500

    More

--- public/js/app.js
+++ public/js/app.js
@@ -33,8 +33,42 @@
                     controller : 'profileController as user'
                 });
         
+            function redirect($q, $injector, auth, store, $location) {
+                return {
+                    responseError: function(rejection) {
+                        if (rejection.status === 401) {
+                            auth.signout();
+                            store.remove('profile');
+                            store.remove('id_token');
+                            $location.path('/home');
+                        }
+                        
+                        return $q.reject(rejection);
+                    }
+                }
+            }
+        
+            $provide.factory('redirect', redirect);
+            $httpProvider.interceptors.push('redirect');
             $httpProvider.interceptors.push('jwtInterceptor');
         
         })
+    
+        .run(function ($rootScope, auth, store, jwtHelper, $location) {
+            
+            $rootScope.$on('$locationChangeStart', function () {
+                
+                var token = store.get('id_token');
+                if (token) {
+                    if (!jwtHelper.isTokenExpired(token)) {
+                        if (!auth.isAuthenticated) {
+                            auth.authenticate(store.get('profile'), token);
+                        }
+                    }
+                } else {
+                    $location.path('/home');
+                }
+            });
+        });
     
 })();
